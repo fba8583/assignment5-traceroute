6b1ec3ea00c6f7effa46af5221d2e6fbeee10ed9
fba8583 <98762381+fba8583@users.noreply.github.com>
2022-04-17 01:37:35 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -126,14 +126,14 @@ def get_route(hostname):
                     bytes = struct.calcsize("d")
                     if types == 11: # TTL excceed
                         timeSent = struct.unpack("d", recvPacket[28:36])[0]
-                        print("type 11")
+                        #print("type 11")
                         print(" {} rtt={} ms {}".format(ttl, int((timeReceived - timeSent)*1000), addr[0]))
                     elif types == 3: # dest unreachable
-                        print("type 3")
+                        #print("type 3")
                         timeSent = struct.unpack("d", recvPacket[28:36])[0]
                         print(" {} rtt={} ms {}".format(ttl, int((timeReceived - timeSent)*1000), addr[0]))
                     elif types == 0:
-                        print("type 0")
+                        #print("type 0")
                         timeSent = struct.unpack("d", recvPacket[28:36])[0]
                         print(timeSent)
                         rtt = int((timeReceived - timeSent)*1000)
