f1e20701bda538811d02ff549860e71952a44494
fba8583 <98762381+fba8583@users.noreply.github.com>
2022-04-24 01:19:43 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -124,8 +124,8 @@ def get_route(hostname):
 
                 #Fill in end
                 try:
-                    host = gethostbyaddr(192.168.1.1)
-                    name_or_ip = '{0} ({1})'.format(192.168.1.1, host[0])
+                    host = gethostbyaddr(host_ip)
+                    name_or_ip = '{0} ({1})'.format(host_ip, host[0])
                     tracelist1.append(name_or_ip)
                 #try to fetch the hostname
                     #Fill in start
