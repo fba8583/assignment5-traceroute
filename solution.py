231e305415f2fb8a10938bda661ca0832b9f9fe4
fba8583 <98762381+fba8583@users.noreply.github.com>
2022-04-24 00:07:05 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -145,7 +145,7 @@ def get_route(hostname):
                 # sourceHostname = gethostbyaddr(addr[0][0])
                 # print("SOURCE HOSTNAME = ",sourceHostname)
                 # Fill in end
-            except error:  # if the host does not provide a hostname
+    except error:  # if the host does not provide a hostname
                   # Fill in start
                 sourceHostname = "hostname not returnable"
                 # Fill in end
